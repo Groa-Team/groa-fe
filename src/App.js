6b0e58302ecd2957bfdfbeb8517a13a3691c257e
Brianna Keune <briannakeune@gmail.com>
2020-03-09 09:54:53 -0500

    more responsive

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/login" component={Login} />
+        <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </Switch>
     </div>
