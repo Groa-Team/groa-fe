16bcffb0dfeef4ae9771bce54745d611122be55c
Brianna Keune <briannakeune@gmail.com>
2020-03-09 09:03:28 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Register from "./components/onboarding/Register.js";
+import Login from "./components/onboarding/Login.js";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
     </div>
   );
 }
